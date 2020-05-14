72aaf90033869ab2f08ddf26972308f85089d7ee
18041430 <18041430@myrp.edu.sg>
2020-05-14 14:17:55 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demodatabase/MainActivity.java
@@ -0,0 +1,66 @@
+package sg.edu.rp.c346.demodatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnInsert, btnGetTasks;
+    TextView tvResults;
+    ListView lv;
+    ArrayAdapter aa;
+    ArrayList<Task> task;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnInsert = findViewById(R.id.btnInsert);
+        btnGetTasks = findViewById(R.id.btnGetTasks);
+        tvResults = findViewById(R.id.tvResults);
+        lv = findViewById(R.id.lv);
+
+
+
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DBHelper db = new DBHelper(MainActivity.this);
+                db.insertTask("Submit RJ", "25 Apr 2016");
+                db.close();
+            }
+        });
+
+        btnGetTasks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                ArrayList<String> data = db.getTaskContent();
+                ArrayList<Task> data2 = db.getTasks();
+                db.close();
+
+                aa = new arrayAdapter(MainActivity.this, R.layout.row, data2);
+                lv.setAdapter(aa);
+
+                String txt = "";
+                for(int i = 0; i < data.size(); i++){
+                    Log.d("Database Content", i +". "+data.get(i));
+                    txt += i + ". " + data.get(i) + "\n";
+                }
+                tvResults.setText(txt);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/sg/edu/rp/c346/demodatabase/arrayAdapter.java
@@ -0,0 +1,56 @@
+package sg.edu.rp.c346.demodatabase;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class arrayAdapter extends ArrayAdapter<Task> {
+    private ArrayList<Task> tasks;
+    private Context context;
+    private TextView tvID, tvDesc, tvDate;
+
+    public arrayAdapter(Context context, int resource, ArrayList<Task> objects) {
+        super(context, resource, objects);
+        // Store the food that is passed to this adapter
+        tasks = objects;
+        // Store Context object as we would need to use it later
+        this.context = context;
+    }
+
+    // getView() is the method ListView will call to get the
+    //  View object every time ListView needs a row
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // The usual way to get the LayoutInflater object to
+        //  "inflate" the XML file into a View object
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        // "Inflate" the row.xml as the layout for the View object
+        View rowView = inflater.inflate(R.layout.row, parent, false);
+
+        // Get the TextView object
+        tvID = (TextView) rowView.findViewById(R.id.tvID);
+        tvDate = (TextView) rowView.findViewById(R.id.tvDate);
+        tvDesc = (TextView) rowView.findViewById(R.id.tvDesc);
+
+
+
+        // The parameter "position" is the index of the
+        //  row ListView is requesting.
+        //  We get back the food at the same index.
+        Task current = tasks.get(position);
+        // Set the TextView to show the food
+
+        tvID.setText(current.getId() + "");
+        tvDesc.setText(current.getDescription());
+        tvDate.setText(current.getDate());
+
+        // Return the nicely done up View to the ListView
+        return rowView;
+    }
+}
